62cc04cbfaeba54db0435b6d22d06417e58e92ef
kikee <kikee@LAPTOP-1AE12EDS>
2021-01-21 13:24:30 +0100

    Hemos hecho un aray y hemos conseguido que nos
    indique cual es el numero maximo

--- src/ejerciciosnivel01_2021/EjerciciosNivel01_2021.java
+++ src/ejerciciosnivel01_2021/EjerciciosNivel01_2021.java
@@ -11,11 +11,30 @@ package ejerciciosnivel01_2021;
  */
 public class EjerciciosNivel01_2021 {
 
+    //declaro un array de integers para hacer pruebas
+    
+    int[] listaNumeros = {50, 31, 27, 2, 5, 99};
+    //el array empieza a contar desde el cero (primera posición cero,
+    //el siguiente vale uno...)
+    
+    public int maximo (int[] lista){
+        int auxiliar = lista[0]; //el máximo va a ser el elemento del array
+        
+        for(int i=0; i< lista.length; i++)
+        if (auxiliar < lista[i]){
+            auxiliar = lista[i];
+        }
+        //esto sirve para que el auxiliar se compare con todos los 
+        //numeros y asi podamos encontrar el numero maximo
+        return auxiliar;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EjerciciosNivel01_2021 ejercicio = new EjerciciosNivel01_2021();
+        System.out.println(ejercicio.maximo(ejercicio.listaNumeros));
+        
     }
     
 }
